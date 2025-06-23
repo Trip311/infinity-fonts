961db0bfd7372d861b3fb8f8b2f182abc72bb122
siboniro <siboniro@gmail.com>
2025-06-23 16:13:51 +0300

    fixed folder sstructure and updated configs

---
+++ infinity-fonts/src/app/_components/NavigationBar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "@/styles/NavigationBar.module.scss";
+import infinityImg from "@/app/assets/infinity.png";
+
+function NavigationBar() {
+  return (
+    <header className={styles.navbar}>
+      <div className={styles.logo}>
+        <Link href="/">
+          Home
+          <Image src={infinityImg} alt="Logo" width={40} height={40} />
+          <span>Infinity Fonts</span>
+        </Link>
+      </div>
+      <nav>
+        <Link href="/fonts" className={styles.link}>
+          Fonts
+        </Link>
+        <Link href="/icons" className={styles.link}>
+          Icons
+        </Link>
+      </nav>
+      <input
+        type="text"
+        placeholder="Search fonts or icons..."
+        className={styles.search}
+      />
+    </header>
+  );
+}
+export default NavigationBar;
