9ddc23a2774e6ae315eea36cd93cfe45fa733a20
jonathan <jonjon.barr@gmail.com>
2025-06-25 17:24:53 +0300

    updated component

--- infinity-fonts/src/app/_components/FontFilter.tsx
+++ infinity-fonts/src/app/_components/FontFilter.tsx
@@ -1,4 +1,4 @@
-import styles from '@/styles/components/filterButton.module.scss';
+import styles from '@/styles/components/FontFilter.module.scss'
 
 interface FontFilterProps {
   styles: string[];
