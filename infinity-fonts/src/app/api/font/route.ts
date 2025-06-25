757ecc595489dc50332727ee813fea317b1abed0
elad1122 <elad.alayoff1@gmail.com>
2025-06-25 19:11:32 +0300

    naming

--- infinity-fonts/src/app/api/font/route.ts
+++ infinity-fonts/src/app/api/font/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server';
 import { getAllFonts } from '@/server/services/font.service';
 export async function GET(req: Request) {
   try {
-    const fontNames = await getAllFonts();
-    return NextResponse.json({ success: true, data: fontNames });
+    const fonts = await getAllFonts();
+    return NextResponse.json({ success: true, data: fonts });
   } catch (error: any) {
     return NextResponse.json(
       { success: false, message: error.message || 'Internal server error' },
